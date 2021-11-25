275614721fb9832613a36d3e688101646a2f3640
doradlavijay <94670608+doradlavijay@users.noreply.github.com>
2021-11-25 22:45:25 +0530

    Update sample_code.sh
    practice commit

--- sample_code.sh
+++ sample_code.sh
@@ -6,5 +6,6 @@ echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
 echo "This web hook concept"
+echo "This webhook pratice"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
